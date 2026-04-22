271df91244217b8548ff1d5cdeb7f2e52f5fe830
Portfolio of Projects by Alexis <info@alexiscollier.com>
2026-04-22 13:30:41 -0400

    Update reference citation in cohort_selection.py

--- src/cohort_selection.py
+++ src/cohort_selection.py
@@ -31,7 +31,7 @@ Reference:
   Collier AM, Shalhout SZ.
   Intensive Documentation Index as an All-Cause Mortality Predictor
   in Critically Ill Patients: A Multi-Center External Validation Study.
-  npj Digital Medicine, 2026.
+  Journal of Biomedical Informatics (under review, 2026).
   HiRID dataset: https://physionet.org/content/hirid/1.1.1/
   MIMIC-IV v2.2:  https://physionet.org/content/mimiciv/2.2/
 """
